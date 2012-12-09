339028283f3514752e4bff143ad6769e72d22731
Ricklon <rick.rickanderson@gmail.com>
2012-12-09 00:07:24 -0500

    Updated from review with Mark Sproul.

--- Game.h
+++ Game.h
@@ -2,37 +2,41 @@
 #define Game_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
+	#include "Arduino.h"
 #else
-#include "WProgram.h"
+	#include "WProgram.h"
 #endif
 
 #include <LiquidCrystal.h>
 
-#define GAME_TITLE 0
-#define GAME_INTRO 1
-#define GAME_INSTR 2
-#define GAME_START 3
-#define GAME_PLAY 4
-#define GAME_DEATH 5
-#define GAME_WIN 6
-#define GAME_RESTART 7
+//*	Nov  2,	2012	<MLS> Changed defines to enum
+enum 
+{
+	GAME_TITLE	=	0,
+	GAME_INTRO,
+	GAME_INSTR,
+	GAME_START,
+	GAME_PLAY,
+	GAME_DEATH,
+	GAME_WIN,
+	GAME_RESTART
+};
 
 
-#define WIDTH 16
-#define HEIGHT 2
-#define RED_BG 12
-#define GREEN_BG 14
-#define BLUE_BG 15
+#define	WIDTH		16
+#define	HEIGHT		2
+#define	RED_BG		12
+#define	GREEN_BG	14
+#define	BLUE_BG		15
 
-#define TILT_PINA 16
-#define TILT_PINB 17
-#define TILT_MID 1
-#define TILT_L 0
-#define TILT_R 2
+#define	TILT_PINA	16
+#define	TILT_PINB	17
+#define	TILT_MID	1
+#define	TILT_L		0
+#define	TILT_R		2
 
-extern unsigned long gTime;
-extern long gPrevTime;
+extern unsigned long	gTime;
+extern long	gPrevTime;
 
 extern int tiltAState;
 extern int tiltBState;
@@ -49,9 +53,11 @@ extern int tiltStatus;  //0 = left, 1 = middle ,2 = right
 extern int leftEnergy;
 extern int rightEnergy;
 
-extern char combatBuff[17];
-extern char spaces[17];
-extern char bars[17];
+#define	kCombatBuffLen	17
+
+extern char combatBuff[kCombatBuffLen];
+extern char spaces[kCombatBuffLen];
+extern char bars[kCombatBuffLen];
   
 extern byte squiGhost1L[8];
 extern byte squiGhost1R[8];
--- tiltspirit.ino
+++ tiltspirit.ino
@@ -1,72 +1,78 @@
-/*
-* The game status loop
- */
+//************************************************************************
+//*	
+//*	I made a small tilt game for Arduiuno called "TIlt Spirit"
+//*	
+//*	It uses an Teensy or  Arduino Leonardo with a 3 pin tilt sensor that 
+//*	triggers at 45 degrees, and displays the game on an standard 2x16 LCD screen.
+//*	
+//************************************************************************
+//*	Edit History
+//************************************************************************
+//*	Nov  1,	2012	<ROA> Wrote Tilt Spirit game
+//*	Nov  2,	2012	<MLS> Code review
+//************************************************************************
 #include <LiquidCrystal.h>
 #include "Game.h"
-#include "Memory.h"
 
 
+//************************************************************************
 void setup() 
 {
-  Serial.begin(9600);
-  pinMode(TILT_PINA, INPUT_PULLUP);
-  pinMode(TILT_PINB, INPUT_PULLUP);
-  tiltAState = digitalRead(TILT_PINA);
-  tiltBState = digitalRead(TILT_PINB);
+	pinMode(TILT_PINA, INPUT_PULLUP);
+	pinMode(TILT_PINB, INPUT_PULLUP);
+	tiltAState	=	digitalRead(TILT_PINA);
+	tiltBState	=	digitalRead(TILT_PINB);
 
-  //Enable the LCD layout information
-  lcd.begin(WIDTH, HEIGHT);
-  lcd.createChar(0, squiGhost1L);
-  lcd.createChar(1, squiGhost1R);
-  lcd.createChar(2, squiGhost2L);
-  lcd.createChar(3, squiGhost2R);
-  lcd.createChar(4, squiGhost3L);
-  lcd.createChar(5, squiGhost3R);
+	//Enable the LCD layout information
+	lcd.begin(WIDTH, HEIGHT);
+	lcd.createChar(0, squiGhost1L);
+	lcd.createChar(1, squiGhost1R);
+	lcd.createChar(2, squiGhost2L);
+	lcd.createChar(3, squiGhost2R);
+	lcd.createChar(4, squiGhost3L);
+	lcd.createChar(5, squiGhost3R);
 
 }
 
+//************************************************************************
+//* The game status loop
+//************************************************************************
+//Note: shake to start
 void loop() 
 {
  
-  switch (gameState) 
-{
-  case GAME_TITLE:
-    gameState = gameTitle();  
-    Serial.print("freeMemory()=");
-    Serial.println(freeMemory());
-    break;
+	switch (gameState) 
+	{
+		case GAME_TITLE:
+			gameState	=	gameTitle();  
+			break;
 
-  case GAME_INTRO:
-    gameState = gameIntro();
-   Serial.print("freeMemory()=");
-    Serial.println(freeMemory());  
-    break;
+		case GAME_INTRO:
+			gameState	=	gameIntro();  
+			break;
 
-  case GAME_INSTR:  
-    gameState = gameInstr();  
-    break;
+		case GAME_INSTR:  
+			gameState	=	gameInstr();  
+			break;
 
-  case GAME_START:  
-    gameState = gameStart();  
-    break;
+		case GAME_START:  
+			gameState	=	gameStart();  
+			break;
 
-  case GAME_PLAY:
-    gameState = gamePlay();  
-    break;
+		case GAME_PLAY:
+			gameState	=	gamePlay();  
+			break;
 
-  case GAME_DEATH:
-    gameState = gameDeath(); 
-    break;
+		case GAME_DEATH:
+			gameState	=	gameDeath(); 
+			break;
 
-  case GAME_WIN:  
-    gameState = gameWin(); 
-    break;
+		case GAME_WIN:  
+			gameState	=	gameWin(); 
+			break;
 
-  case GAME_RESTART:
-    gameState = gameRestart();
-    Serial.print("freeMemory()=");
-    Serial.println(freeMemory());
-    break;
-  }
-
+		case GAME_RESTART:
+			gameState	=	gameRestart();
+			break;
+	}
 }
